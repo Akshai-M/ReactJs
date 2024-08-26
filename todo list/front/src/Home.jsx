3cf4097deca975875770578f9a4a788fc9201f1c
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-08-26 23:29:49 +0530

    changes

--- todo list/front/src/Home.jsx
+++ todo list/front/src/Home.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      
+      <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+      <p className="mt-2">This is a React + Vite project with Tailwind CSS.</p>
+    </div>
     </div>
   )
 }
 
 export default Home
-
