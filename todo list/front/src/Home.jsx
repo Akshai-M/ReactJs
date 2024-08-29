17ae806e6600b40943338a800295dbe4b332a023
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-08-29 18:08:16 +0530

    modified

--- todo list/front/src/Home.jsx
+++ todo list/front/src/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import Create from './Create.jsx'
+import axios from 'axios'
 function Home() {
+  const [todos, setTodo] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3001/get')
+    .then(result => setTodo(result.data))
+    .catch(err => console.log(err)
+    )
+  }, [])
+
+
   return (
     <div>
-      <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
-      <p className="mt-2">This is a React + Vite project with Tailwind CSS.</p>
-    </div>
+      <h2>Todo list</h2>
+      <Create />
+      {
+        todos.length === 0
+          ?
+          (<div><h2>No Record</h2></div>)
+          :
+          (todos.map(todo => {
+            <div>
+              {todo.task}
+            </div>
+          }))
+      }
     </div>
   )
 }
