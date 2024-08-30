b738d80313b8bfafd939ab8ffc3ebf4b1465d5d7
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-08-30 23:04:47 +0530

    modified

--- todo list/front/src/Home.jsx
+++ todo list/front/src/Home.jsx
@@ -1,33 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import Create from './Create.jsx'
-import axios from 'axios'
-function Home() {
-  const [todos, setTodo] = useState([])
-  useEffect(() => {
-    axios.get('http://localhost:3001/get')
-    .then(result => setTodo(result.data))
-    .catch(err => console.log(err)
-    )
-  }, [])
+import React, { useState, useEffect } from 'react';
+import Create from './Create.jsx';
+import axios from 'axios';
 
+function Home() {
+  const [todos, setTodo] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/get');
+        setTodo(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h2>Todo list</h2>
       <Create />
-      {
-        todos.length === 0
-          ?
-          (<div><h2>No Record</h2></div>)
-          :
-          (todos.map(todo => {
-            <div>
-              {todo.task}
+      <br />
+      {todos.length === 0 ? (
+        <div><h2>Loading...</h2></div>
+      ) : (
+        todos.map((todo, index) => (
+          <div key={todo.id || index} className='flex border border-red-500 max-w-xs '>
+            <div className='flex '>
+              <input type="checkbox"/>
+              <p>{todo.task}</p>
             </div>
-          }))
-      }
+            <div className= 'cursor-pointer'>
+              &#9888;
+            </div>
+          </div>
+        ))
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
