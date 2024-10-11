c23cc9e90c154ece87aa96a299c0d67ee970204d
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-10-11 19:16:23 +0530

    reactRouter

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-black bg-green-600 p-4'>React Router</h1>
+    </>
+  )
+}
+
+export default App
