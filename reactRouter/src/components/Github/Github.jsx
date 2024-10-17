6bcf4c9d1950ade289d48bc4a4c6a8bc2b953264
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-10-17 11:29:47 +0530

    GithubPage

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('http://api.github.com/users/akshai-m')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setData(data)
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('http://api.github.com/users/akshai-m')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
             
-        })
-    }, [])
+    //     })
+    // }, [])
   return (
     <div className='text-center m-4 text-black font-mono p-4 text-3xl'>Github followers: {data.followers}
     <img className='rounded-full mx-auto' src={data.avatar_url} alt="Git picture" width={300} />
@@ -20,4 +20,10 @@ function Github() {
   )
 }
 
+export default Github
 
+export const githubInfoLoader = async() => {
+
+    const response = await fetch('https://api.github.com/users/akshai-m')
+    return response.json()
+}
