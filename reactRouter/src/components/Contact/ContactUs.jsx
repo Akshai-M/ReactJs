4e74b2bf1645ad49d60d34c3e8c890c831846184
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-10-18 11:47:33 +0530

    Contact

--- reactRouter/src/components/Contact/ContactUs.jsx
+++ reactRouter/src/components/Contact/ContactUs.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 export default function Contact() {
     return (
-        
-                        
+        <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
+            <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div className="mt-8 overflow-hidden">
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <div className="p-6 mr-2 bg-gray-100 sm:rounded-lg">
+                            <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
+                                Get in touch: 
+                            </h1>
+                            <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
+                                Fill in the form to start a conversation
+                            </p>
+
+                            
+                            
+
+                            <div className="flex items-center mt-2 text-gray-600">
+                                <svg
+                                    fill="none"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    viewBox="0 0 24 24"
+                                    className="w-8 h-8 text-gray-500"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="1.5"
+                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                                    />
+                                </svg>
+                                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                                    info@acme.org
+                                </div>
+                            </div>
+                        </div>
 
                         <form className="p-6 flex flex-col justify-center">
                             <div className="flex flex-col">
@@ -51,6 +86,9 @@ export default function Contact() {
                                 Submit
                             </button>
                         </form>
-                    
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
